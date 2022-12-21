99e53fbca13fb2b27c126fb89c120a60b19b3c20
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-21 22:45:51 +0530

    create file to import all image files

---
+++ src/components/importImages.js
@@ -0,0 +1,123 @@
+import c0 from "../assets/0.png";
+import c1 from "../assets/1.png";
+import c2 from "../assets/2.png";
+import c3 from "../assets/3.png";
+import c4 from "../assets/4.png";
+import c5 from "../assets/5.png";
+import c6 from "../assets/6.png";
+import c7 from "../assets/7.png";
+import c8 from "../assets/8.png";
+import c9 from "../assets/9.png";
+import c10 from "../assets/10.png";
+import c11 from "../assets/11.png";
+import c12 from "../assets/12.png";
+import c13 from "../assets/13.png";
+import c14 from "../assets/14.png";
+import c15 from "../assets/15.png";
+import c16 from "../assets/16.png";
+import c17 from "../assets/17.png";
+import c18 from "../assets/18.png";
+import c19 from "../assets/19.png";
+import c20 from "../assets/20.png";
+import c21 from "../assets/21.png";
+import c22 from "../assets/22.png";
+import c23 from "../assets/23.png";
+import c24 from "../assets/24.png";
+import c25 from "../assets/25.png";
+import c26 from "../assets/26.png";
+import c27 from "../assets/27.png";
+import c28 from "../assets/28.png";
+import c29 from "../assets/29.png";
+import c30 from "../assets/30.png";
+import c31 from "../assets/31.png";
+import c32 from "../assets/32.png";
+import c33 from "../assets/33.png";
+import c34 from "../assets/34.png";
+import c35 from "../assets/35.png";
+import c36 from "../assets/36.png";
+import c37 from "../assets/37.png";
+import c38 from "../assets/38.png";
+import c39 from "../assets/39.png";
+import c40 from "../assets/40.png";
+import c41 from "../assets/41.png";
+import c42 from "../assets/42.png";
+import c43 from "../assets/43.png";
+import c44 from "../assets/44.png";
+import c45 from "../assets/45.png";
+import c46 from "../assets/46.png";
+import c47 from "../assets/47.png";
+import c48 from "../assets/48.png";
+import c49 from "../assets/49.png";
+import c50 from "../assets/50.png";
+import c51 from "../assets/51.png";
+import c52 from "../assets/52.png";
+import c53 from "../assets/53.png";
+import c54 from "../assets/54.png";
+import c55 from "../assets/55.png";
+import c56 from "../assets/56.png";
+import c57 from "../assets/57.png";
+import c58 from "../assets/58.png";
+
+const allImages = [
+  c0,
+  c1,
+  c2,
+  c3,
+  c4,
+  c5,
+  c6,
+  c7,
+  c8,
+  c9,
+  c10,
+  c11,
+  c12,
+  c13,
+  c14,
+  c15,
+  c16,
+  c17,
+  c18,
+  c19,
+  c20,
+  c21,
+  c22,
+  c23,
+  c24,
+  c25,
+  c26,
+  c27,
+  c28,
+  c29,
+  c30,
+  c31,
+  c32,
+  c33,
+  c34,
+  c35,
+  c36,
+  c37,
+  c38,
+  c39,
+  c40,
+  c41,
+  c42,
+  c43,
+  c44,
+  c45,
+  c46,
+  c47,
+  c48,
+  c49,
+  c50,
+  c51,
+  c52,
+  c53,
+  c54,
+  c55,
+  c56,
+  c57,
+  c58,
+];
+
+export default allImages;
