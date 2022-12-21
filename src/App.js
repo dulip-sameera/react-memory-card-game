a61cca9f38160d3eacdbfef730657358c8198af3
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-21 14:30:34 +0530

    add header and body sections

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Header, Body } from "./components";
 
 const App = () => {
-  return <div>App</div>;
+  const cards = [1, 2, 3, 4, 5, 6, 7, 8];
+  const handleCardClick = (e) => console.log(e.target.id);
+  return (
+    <div className="h-screen">
+      <Header score="0" bestScore="0" />
+      <Body cards={cards} handleCardClick={handleCardClick} />
+    </div>
+  );
 };
 
 export default App;
