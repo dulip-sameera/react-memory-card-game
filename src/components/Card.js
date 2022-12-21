6debb8fc14f58461f2a2f907d3fc6c7cab8a86a9
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-21 22:46:23 +0530

    add card styles and content

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,15 @@
 import React from "react";
-
-const Card = () => {
-  return <div>Card</div>;
+import allImages from "./importImages";
+const Card = ({ id, handleCardClick }) => {
+  return (
+    <div
+      className="w-[300px] h-[300px] flex items-center justify-center  bg-CustomBlue-500 rounded-lg duration-500 active:scale-110 active:bg-CustomBlue-100 md:hover:scale-110 md:hover:cursor-pointer md:hover:bg-CustomBlue-100"
+      id={id}
+      onClick={handleCardClick}
+    >
+      <img src={allImages[id]} alt="" id={id} />
+    </div>
+  );
 };
 
 export default Card;
